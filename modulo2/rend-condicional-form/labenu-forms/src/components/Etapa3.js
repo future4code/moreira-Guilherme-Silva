6da24db0b89a05004af980a3c1ce27c506b9e687
Guilherme <guisilva.207@gmail.com>
2022-01-17 18:22:53 -0300

    Exercícios de renderização condicional

---
+++ modulo2/rend-condicional-form/labenu-forms/src/components/Etapa3.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+
+
+function Etapa3() {
+    return (
+      <div>
+        <h3>ETAPA 3 - INFORMAÇÕES GERAIS DE ENSINO</h3>
+        <p>5. Por que você não terminou um curso de graduação?</p>
+        <input />
+        <p>6. Você fez algum curso complementar?</p>
+        <select>
+            <option value="Nenhum">Nenhum</option>
+            <option value="Curso Técnico">Curso Técnico</option>
+            <option value="Curso de Inglês">Curso de Inglês</option>
+        </select>
+
+      </div>
+    );
+  }
+  
+  export default Etapa3;
+
+
