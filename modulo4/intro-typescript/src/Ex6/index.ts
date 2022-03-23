7f36e5786a46d0460d1bef837b546a3de386b922
Guilherme Cruz <guilhermecruz@MacBook-Air-de-Guilherme.local>
2022-03-23 17:02:12 -0300

    Exercícios de typescript

---
+++ modulo4/intro-typescript/src/Ex6/index.ts
@@ -0,0 +1,11 @@
+function operacoes(num1: number, num2: number): void {
+    console.log(num1 + num2)
+    console.log(num1 - num2)
+    console.log(num1 * num2)
+
+    if(num1 >= num2){
+        console.log(num1)
+    } else {
+        console.log(num2)
+    }
+}
